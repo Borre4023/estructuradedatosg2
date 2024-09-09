5779b9bdf9f32abd160a2c80f8f48cd09fce4340
Alexander Domínguez Zárate <alexanderdominguez11723@gmail.com>
2024-09-09 14:54:20 -0600

    Logro: Creación de Menu y programa de Hola Mundo Recursivo

--- 1.1_proyecto_VS/01-conceptos-basicos/src/edu/mx/uttt/recursividad/Operaciones.java
+++ 1.1_proyecto_VS/01-conceptos-basicos/src/edu/mx/uttt/recursividad/Operaciones.java
@@ -2,4 +2,64 @@ package edu.mx.uttt.recursividad;
 
 public class Operaciones {
 
+        private int n;
+
+        public Operaciones(){
+            this.n = 1;
+        }
+
+        public int getN() {
+            return n;
+        }
+
+        public void setN(int n) {
+            this.n = (n>0)?n:1;
+        }
+
+        //MËTODO ITERATIVO
+
+        public void imprimir(){
+            int i = 1;
+            while(i<=n){
+                System.out.println("Hola Mundo");
+                i++;
+            }
+            
+        }
+
+        //MÉTODO INTERACTIVO 2
+
+        public void imprimir2(){
+            int i = 1;
+            while (i<=n){
+                System.out.println(n);
+                i++;
+            }
+        }
+
+        public void imprimir(int n){
+            //CASO BASE
+
+            if(n == 1){
+                System.out.println("Hola Mundo");
+            }else{
+                //CASO GENERAL
+                System.out.println("Hola Mundo");
+                imprimir(n-1);
+            }
+        }
+
+        public void imprimir2(int n){
+            //CASO BASE 2
+
+            if(n == 1){
+                System.out.println(n);
+            }else{
+                //CASO GENERAL 2
+                System.out.println(n);
+                imprimir2(n-1);
+            }
+        }
+
+        
 }
--- 1.1_proyecto_VS/01-conceptos-basicos/src/edu/mx/uttt/recursividad/TestRecursividad.java
+++ 1.1_proyecto_VS/01-conceptos-basicos/src/edu/mx/uttt/recursividad/TestRecursividad.java
@@ -1,5 +1,62 @@
 package edu.mx.uttt.recursividad;
 
-public class TestRecursividad {
+import javax.swing.JOptionPane;
 
+public class TestRecursividad {
+    public static void main(String[] args) {
+        ejecutar();
+    }
+    public static String menu(){
+        String menu= "Menu Principal\n" +
+                "1) Método Iterativo\n"+
+                "2) Método Recursivo\n"+
+                "3) Salir\n"+
+                "Elige la opción";
+        return JOptionPane.showInputDialog(menu);
+    }
+
+    public static void ejecutar(){
+        boolean sentinel = true;
+        String opcion ="";
+        Operaciones op = new Operaciones();
+        int n=0;
+
+            do{
+
+                opcion = menu();
+                
+            
+        switch (opcion) {
+            case "1":
+                n= Integer.parseInt(JOptionPane.showInputDialog("Introcude el número de veces a imprimir"));
+                op.setN(n);
+                op.imprimir();
+            break;
+
+            
+            case "2":
+                n= Integer.parseInt(JOptionPane.showInputDialog("Introcude el número de veces a imprimir"));
+                op.setN(n);
+                op.imprimir(op.getN());
+            break;
+
+            
+            case "3": //Salir
+                sentinel = false;
+                JOptionPane.showMessageDialog(null, "I'll be back\n");
+                JOptionPane.showMessageDialog(null, "tu mama");
+            break;
+
+        
+            default:
+            JOptionPane.showMessageDialog(null, "Valor no valido");
+                break;
+        }//Cierra el switch
+
+        }while(sentinel); //Cierra el do-while
+
+    } //Cierra el método
+    
 }
+
+
